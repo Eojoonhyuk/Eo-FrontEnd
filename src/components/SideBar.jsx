0b3d9c6a3df68f9f82fc00f7120b075cf77d79fb
Eojoonhyuk <94384392+Eojoonhyuk@users.noreply.github.com>
2023-02-16 00:29:35 +0900

    feat: SideBar 레이아웃 초안 완료

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,15 @@
 import styled from "styled-components";
-import { 
-    AiFillHome, 
-    IoPaperPlaneOutline, 
-    AiOutlineHeart, 
-    FiPlusSquare, 
-    FaUserCircle,
-    AiOutlineMenu
-} from "react-icons/ai";
+import { AiFillHome, AiOutlineHeart, AiOutlineMenu } from "react-icons/ai";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { FiPlusSquare } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
 
 const Items = [
-    { id: "1", icon: "AiFillHome", content: "홈" },
-    { id: "2", icon: "IoPaperPlaneOutline", content: "메시지" },
-    { id: "3", icon: "AiOutlineHeart", content: "알림" },
-    { id: "4", icon: "FiPlusSquare", content: "만들기" },
-    { id: "5", icon: "FaUserCircle", content: "프로필" },
+    { id: "1", icon: <AiFillHome />, content: "홈" },
+    { id: "2", icon: <IoPaperPlaneOutline />, content: "메시지" },
+    { id: "3", icon: <AiOutlineHeart />, content: "알림" },
+    { id: "4", icon: <FiPlusSquare />, content: "만들기" },
+    { id: "5", icon: <FaUserCircle />, content: "프로필" },
 ]
 
 
@@ -26,9 +22,7 @@ const SideBar = () => {
             <ItemArea>
             {Items.map((item)=>(
                 <ItemBox key={item.id}>
-                    <ItemIconBox>
-                        <ItemIcon>{item.icon}</ItemIcon>
-                    </ItemIconBox>
+                    <ItemIconBox>{item.icon}</ItemIconBox>
                     <ItemContent>{item.content}</ItemContent>
                 </ItemBox>
             ))}
@@ -51,36 +45,53 @@ const SideBarArea = styled.div`
     width: 250px;
     height: 100%;
     border-right: 1px solid #ced4da;
-    padding: 2rem 1rem;
+    padding: 2rem 1rem 2rem 1.5rem;
 `;
 
 const TopBottomBox = styled.div`
     width: 60%;
     padding: 1rem 0;
+    :last-child{
+        width: 100%;
+        padding: 0.2rem 0;
+        :hover{
+            border-radius: 20px;
+            background-color: #f8f9fa;
+        }
+    }
 `;
 
 const SideBarImg = styled.img`
     width: 100%;
+    cursor: pointer;
 `;
 
 const ItemArea = styled.div`
-    height: 80%;
+    height: 85%;
 `
 
 const ItemBox = styled.div`
     display: flex;
     align-items: center;
     height: 8%;
-`;
+    font-size: 1.3rem;
+    padding: 0.3rem;
+    cursor: pointer;
+    :hover{
+        border-radius: 20px;
+        background-color: #f8f9fa;
+    }
+    `;
 
 const ItemIconBox = styled.div`
-    
+    font-size: 1.6rem;
+    margin-right: 1.2rem;
+    :hover{
+        transform: scale(1.1);
+    }
 `;
 
-const ItemIcon = styled.div`
-    
-`;
 
 const ItemContent = styled.div`
-
+    padding-bottom: 0.3rem;
 `;
